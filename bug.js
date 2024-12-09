14a97f1370a2d20571c75ac615fae8960e7c2d72
BugHunter-X <askzenai@gmail.com>
2024-12-09 22:38:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to use a component's state or props before it has been mounted. This often happens when you try to access state or props in the component's constructor or in a function called before the component has rendered.
